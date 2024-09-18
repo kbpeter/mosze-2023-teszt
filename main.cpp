74c5c52e10a49db87b2d708c8f82f9be6b49ee22
kbpeter <p.boldi.kovacs@gmail.com>
2024-09-18 19:14:13 +0200

    comments fatal errors

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //nem egyező változó név
+    std::cout << '1-100 ertekek duplazasa' //nincs ;
+    for (int i = 0;) //nincs befjezve a for header
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
         std::cout << "Ertek:"
-    }
+    } //nincs ;
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // , = ;
     {
         atlag += b[i]
-    }
+    } //nincs ;
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
